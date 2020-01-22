914b18c5269f90d7bb1d58b7b643351ac53012a8
Mohamedelmahfoud2019 <58991112+Mohamedelmahfoud2019@users.noreply.github.com>
2020-01-22 16:22:57 +0100

    PROGRAMA_3

--- PROGRAMA_3/PROGRAMA_3.ino
+++ PROGRAMA_3/PROGRAMA_3.ino
@@ -1,4 +1,4 @@
-const int led0 = 5;          // donar nom al pin 5 de l’Arduino
+const int led0 = 5;          // donar nom al pin 5 de l’Arduino  
 const int led1 = 6;          // donar nom al pin 6 de l’Arduino
 const int led2 = 7;          // donar nom al pin 7 de l’Arduino
 const int led3 = 8;          // donar nom al pin 8 de l’Arduino
@@ -26,7 +26,7 @@ void loop()
   if (digitalRead(buttonPin) == 1)
 
   {
-   digitalWrite(led0, HIGH);    // posar a 5V el pin 5
+  digitalWrite(led0, HIGH);    // posar a 5V el pin 5
   digitalWrite(led1,LOW);    // posar a 5V el pin 6
   digitalWrite(led2, HIGH);    // posar a 5V el pin 7
   digitalWrite(led3, LOW);    // posar a 5V el pin 8
